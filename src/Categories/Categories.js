de206be711b4c4da7f486f09cd93c6dbf03f4cef
korayerdem89 <developerkoray@gmail.com>
2022-07-11 00:27:31 +0300

    flatlist ile renderlama yapıldı

--- src/Categories/Categories.js
+++ src/Categories/Categories.js
@@ -1,27 +1,38 @@
-import React, {useState, useEffect} from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, SafeAreaView, FlatList } from 'react-native';
 import axios from 'axios';
 import config from '../../config';
 
+
 const Categories = () => {
     const [text, setText] = useState("Koray");
-    
+    const [data, setData] = useState([]);
+    console.log(data.map(item => item["idCategory"]));
+
     const fetchApi = async () => {
         try {
-         const {data: responseData} = await axios.get(config.API_MAIN_URL);
-         console.log(responseData);
-        }catch(error) {
+            const { data: responseData } = await axios.get(config.API_MAIN_URL);
+            setData(responseData.categories)
+        } catch (error) {
             console.log(error.message);
         }
     }
-    
+
+    const renderApi = ({ item }) =>
+        <Text style={{ color: "black", backgroundColor: "#e0e0e0" }}>{item["strCategory"]}</Text>
+
+
     useEffect(() => {
         fetchApi();
-      }, []);
-    
+    }, []);
+
     return (
-        <SafeAreaView>
-            <Text>Hello {text}</Text>
+        <SafeAreaView style={{ flex: 1 }}>
+            <FlatList
+                keyExtractor={(item, _index) => item.idCategory}
+                data={data}
+                renderItem={renderApi}
+            />
         </SafeAreaView>
     );
 };
