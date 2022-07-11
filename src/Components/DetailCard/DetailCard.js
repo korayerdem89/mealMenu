b320cfaacc72f9084a206733e0c4f41e483af6dc
korayerdem89 <developerkoray@gmail.com>
2022-07-11 15:38:51 +0300

    detailcard componenti oluşturulup tasarımı tamamlandı

---
+++ src/Components/DetailCard/DetailCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { TouchableWithoutFeedback, Text, Image, View, TouchableOpacity } from 'react-native';
+import styles from './DetailCard.style';
+
+const DetailCard = ({ data, onSelect }) => {
+    return (
+        <TouchableWithoutFeedback onPress={onSelect}>
+            <View style={styles.flatContainer}>
+                <Image style={styles.image} source={{ uri: data.strMealThumb }} />
+                <View style={styles.textContainer}>
+                    <Text style={styles.mealTitle}>{data.strMeal}</Text>
+                    <Text style={styles.mealArea}>{data.strArea}</Text>
+                    <Text style={styles.mealInstructions}>{data.strInstructions}</Text>
+                </View>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Watch on Youtube</Text>
+                </TouchableOpacity>
+            </View>
+        </TouchableWithoutFeedback >
+    )
+};
+
+
+export default DetailCard;
+
+
--- src/Detail/Detail.js
+++ src/Detail/Detail.js
@@ -1,27 +1,17 @@
-import React, { useState } from 'react';
-import { FlatList, SafeAreaView, Text, View, Image, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { FlatList, SafeAreaView } from 'react-native';
 import styles from './Detail.style';
 import useFetch
     from '../hooks/useFetch/useFetch';
 import config from '../../config';
-
+import DetailCard from '../Components/DetailCard';
 
 const Detail = ({ route, navigation }) => {
     const { meal } = route.params;
     const { data } = useFetch(`${config.API_DETAIL_URL}${meal}`);
 
     const renderCategory = ({ item }) =>
-        <View style={styles.flatContainer}>
-            <Image style={styles.image} source={{ uri: item.strMealThumb }} />
-            <View style={styles.textContainer}>
-                <Text style={styles.mealTitle}>{item.strMeal}</Text>
-                <Text style={styles.mealArea}>{item.strArea}</Text>
-                <Text style={styles.mealInstructions}>{item.strInstructions}</Text>
-            </View>
-            <TouchableOpacity style={styles.button}>
-           <Text style={styles.buttonText}>Watch on Youtube</Text>
-            </TouchableOpacity>
-        </View>
+        <DetailCard data={item} />
 
 
 
