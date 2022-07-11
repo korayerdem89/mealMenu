a252d360da1cc57cd4e800ae7afc39a0d459a145
korayerdem89 <developerkoray@gmail.com>
2022-07-11 15:08:52 +0300

    Detail sayfasında fetchleme başarıyla yapıldı

--- src/Detail/Detail.js
+++ src/Detail/Detail.js
@@ -1,13 +1,27 @@
-import React, {useState} from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, SafeAreaView, Text, View, Image } from 'react-native';
+import styles from './Detail.style';
+import useFetch
+    from '../hooks/useFetch/useFetch';
+import config from '../../config';
+ 
+
+const Detail = ({ route, navigation }) => {
+    const { meal } = route.params;
+    const { data } = useFetch(`${config.API_DETAIL_URL}${meal}`);
+
+     const renderCategory = ({ item }) =>
+       <Text>{item.strMeal}</Text>
 
 
-const Detail = () => {
-    const [text, setText] = useState("Koray");
 
     return (
-        <SafeAreaView>
-            <Text>Hello {text}</Text>
+        <SafeAreaView style={styles.container}>
+            <FlatList
+                keyExtractor={(item, _index) => item.idMeal}
+                data={data.meals}
+                renderItem={renderCategory}
+            />
         </SafeAreaView>
     );
 };
