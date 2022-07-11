95a30d5382f7c25a676d56b0a369b59ebc2f77ed
korayerdem89 <developerkoray@gmail.com>
2022-07-11 15:30:45 +0300

    Detail sayfa tasarımı tamamlandı

--- src/Detail/Detail.js
+++ src/Detail/Detail.js
@@ -1,17 +1,27 @@
 import React, { useState } from 'react';
-import { FlatList, SafeAreaView, Text, View, Image } from 'react-native';
+import { FlatList, SafeAreaView, Text, View, Image, TouchableOpacity } from 'react-native';
 import styles from './Detail.style';
 import useFetch
     from '../hooks/useFetch/useFetch';
 import config from '../../config';
- 
+
 
 const Detail = ({ route, navigation }) => {
     const { meal } = route.params;
     const { data } = useFetch(`${config.API_DETAIL_URL}${meal}`);
 
-     const renderCategory = ({ item }) =>
-       <Text>{item.strMeal}</Text>
+    const renderCategory = ({ item }) =>
+        <View style={styles.flatContainer}>
+            <Image style={styles.image} source={{ uri: item.strMealThumb }} />
+            <View style={styles.textContainer}>
+                <Text style={styles.mealTitle}>{item.strMeal}</Text>
+                <Text style={styles.mealArea}>{item.strArea}</Text>
+                <Text style={styles.mealInstructions}>{item.strInstructions}</Text>
+            </View>
+            <TouchableOpacity style={styles.button}>
+           <Text style={styles.buttonText}>Watch on Youtube</Text>
+            </TouchableOpacity>
+        </View>
 
 
 
